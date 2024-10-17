4dd0da6df71f7dc8648d786c5eec4311c39da94d
Bohdan <bogdan7450@gmail.com>
2024-10-17 19:59:45 +0300

    Updated fixtures

--- conftest.py
+++ conftest.py
@@ -10,26 +10,24 @@ headless = False
 @pytest.fixture(scope="session")
 def browser(request):
     with sync_playwright() as p:
-        browser =  getattr(p, request.param).launch(headless=headless)
+        browser = getattr(p, request.param).launch(headless=headless)
         yield browser
         browser.close()
 
 
 @pytest.fixture(scope="function", autouse=True)
-def attach_allure_artifacts(request):
+def test_artifacts(request):
     yield
     if request.node.rep_call.failed:
-        allure.attach.file(f"traces/{request.node.name}.zip", name="trace", attachment_type="application/zip")
         allure.attach.file(f"screenshots/{request.node.name}.png", name="screenshot",
                            attachment_type=allure.attachment_type.PNG)
+        allure.attach.file(f"videos/{request.node.name}.webm", name="video", attachment_type=allure.attachment_type.WEBM)
 
 
 @pytest.fixture(scope="function")
 def context(request, browser):
     context = browser.new_context(record_video_dir="videos/")
-    context.tracing.start(screenshots=True, snapshots=True, sources=True)
     yield context
-    context.tracing.stop(path=f"traces/{request.node.name}.zip")
     context.close()
 
 
@@ -39,9 +37,10 @@ def page(context, request):
     yield page
     if request.node.rep_call.failed:
         page.screenshot(path=f"screenshots/{request.node.name}.png", full_page=True)
-        video_path = page.video.path()
-        allure.attach.file(video_path, name="video", attachment_type=allure.attachment_type.WEBM)
     page.close()
+    if request.node.rep_call.failed:
+        page.video.save_as(path=f"videos/{request.node.name}.webm")
+
 
 @pytest.fixture(name="config", scope="session", autouse=True)
 def config_fixture():
--- tests/tests_main_page.py
+++ tests/tests_main_page.py
@@ -2,7 +2,7 @@ import pytest
 from base.base_test import BaseTest
 
 
-@pytest.mark.parametrize("browser", ["firefox", "chromium"], indirect=True)
+@pytest.mark.parametrize("browser", ["chromium"], indirect=True)
 class TestMainPage(BaseTest):
 
     @pytest.fixture(autouse=True, scope='function')
@@ -10,8 +10,11 @@ class TestMainPage(BaseTest):
         self.main_page.open_main_page()
         yield
 
-    def test_title(self, config):
-        assert config.BASE_URL == self.main_page.get_url()
+    def test_failed(self, config):
+        self.main_page.click_elements_card()
+        self.main_page.open_main_page()
+        self.main_page.click_forms_card()
+        assert config.BASE_URL == "self.main_page.get_url()"
 
     def test_main(self, config):
         assert config.TITLE == self.main_page.title()
